464d5e0819fb59ffbbc9af33e810a89b90b25187
nkenned3 <nkenned3@gmail.com>
2018-04-22 17:51:44 -0700

    attempting to commit to remote server

---
+++ BirthdayCard/Models/SendCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class SendCard
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string Sender { get; set; }
+        [Required(ErrorMessage = "Please enter To")]
+        public string Recipient { get; set; }
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+    }
+}
